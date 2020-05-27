0c9feb3e649aa817b952b61254b96feb758068c4
Rcmcpe <rcmcpe@qq.com>
2020-05-27 22:22:02 +0800

    在入口类添加Exception处理器

--- Auto Arknights CLI/Entry.cs
+++ Auto Arknights CLI/Entry.cs
@@ -1,11 +1,24 @@
-﻿namespace REVUnit.AutoArknights.CLI
+﻿using System;
+using REVUnit.AutoArknights.Core;
+using REVUnit.Crlib.Extension;
+
+namespace REVUnit.AutoArknights.CLI
 {
     public static class Entry
     {
         public static void Main()
         {
-            using var app = new AutoArknights();
-            app.Run();
+            try
+            {
+                using var app = new AutoArknights();
+                app.Run();
+            }
+            catch (Exception e)
+            {
+                Log.Error(e.Message, withTime: true);
+                Log.Debug(e.StackTrace);
+                XConsole.AnyKey();
+            }
         }
     }
 }
