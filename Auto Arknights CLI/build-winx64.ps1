cad5b47a92778b76a5df7aa278660c3526e37825
Rcmcpe <rcmcpe@qq.com>
2020-07-14 23:25:58 +0800

    修复构建脚本（狗*的vscode）

--- Auto Arknights CLI/build-winx64.ps1
+++ Auto Arknights CLI/build-winx64.ps1
@@ -2,8 +2,8 @@ dotnet publish -c Release -v n
 Remove-Item artifact\*.pdb
 $depsFile = 'artifact\Auto Arknights CLI.deps.json'
 $deps = [System.IO.File]::ReadAllText($depsFile).Replace('
-"runtimes/win-x64/native/OpenCvSharpExtern.pdb": {
-  "fileVersion": "0.0.0.0"
-},', [System.String]::Empty)
+          "runtimes/win-x64/native/OpenCvSharpExtern.pdb": {
+            "fileVersion": "0.0.0.0"
+          },', [System.String]::Empty)
 [System.IO.File]::WriteAllText($depsFile, $deps)
 Pause
