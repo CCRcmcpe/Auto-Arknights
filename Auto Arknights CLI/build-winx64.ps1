d900eae29e850f5f140a93ea0e2833c9cf19b16a
Rcmcpe <rcmcpe@qq.com>
2020-08-27 03:25:46 +0800

    Improve build script

--- Auto Arknights CLI/build-winx64.ps1
+++ Auto Arknights CLI/build-winx64.ps1
@@ -1,5 +1,6 @@
 dotnet publish -c Release -v n
 Remove-Item artifact\*.pdb
+Remove-Item -R artifact\dll
 $depsFile = 'artifact\Auto Arknights CLI.deps.json'
 $deps = [System.IO.File]::ReadAllText($depsFile).Replace('
           "runtimes/win-x64/native/OpenCvSharpExtern.pdb": {
