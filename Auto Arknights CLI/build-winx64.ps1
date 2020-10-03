24f8e786e8718c98114b7a69a3c87f18fe43ae28
Rcmcpe <rcmcpe@qq.com>
2020-10-03 15:56:45 +0800

    修复构建脚本的路径问题

--- Auto Arknights CLI/build-winx64.ps1
+++ Auto Arknights CLI/build-winx64.ps1
@@ -1,7 +1,7 @@
 dotnet publish -c Release -v n
-Remove-Item artifact\*.pdb
-Remove-Item -R artifact\dll
-$depsFile = 'artifact\Auto Arknights CLI.deps.json'
+Remove-Item artifact/*.pdb
+Remove-Item -R artifact/dll
+$depsFile = [System.IO.Path]::Combine($pwd, 'artifact/Auto Arknights CLI.deps.json')
 $deps = [System.IO.File]::ReadAllText($depsFile).Replace('
           "runtimes/win-x64/native/OpenCvSharpExtern.pdb": {
             "fileVersion": "0.0.0.0"
