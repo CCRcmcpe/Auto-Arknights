09c1e48c5c21b8962bc3325999ba8da5478e834c
Rcmcpe <rcmcpe@qq.com>
2021-02-06 02:34:30 +0800

    移动Logo至资源文件，添加帮助

--- Auto Arknights CLI/App.cs
+++ Auto Arknights CLI/App.cs
@@ -1,15 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Text;
+using REVUnit.AutoArknights.CLI.Properties;
 using REVUnit.AutoArknights.Core;
 using REVUnit.AutoArknights.Core.Tasks;
 using REVUnit.Crlib.Extension;
 using REVUnit.Crlib.Input;
 using Serilog;
-using Serilog.Events;
 
 namespace REVUnit.AutoArknights.CLI
 {
@@ -17,87 +16,73 @@ namespace REVUnit.AutoArknights.CLI
     {
         private const string ConfigFilePath = "Auto Arknights CLI.config.json";
 
-        private const string Logo =
-            @"                                                                                                    
-                                   ,                                                                
-            (                      @@(                                       .                      
-            @@@       /            @@@(  /@,                                  @@@.                  
-            @@@  @@@  @@@          @@@/        (@@@                       @@/ @@                    
-            @@@  @@@  @@@   ,@@@   @@@/        (@@@        @(             @@@    (@*                
-            @@@  @@@  @@@   ,@@@   @@@/        (@@@         @@@           @@@  @@%   @@@            
-            @@@ .@@@  @@@*  ,@@@   @@@@#&.     (@@@ .%@@#    @@%          @@@   @@@  @@@            
-            @@@  @@@  @@@   ,@@@   @@@/        @@@@       @@@//@*         @@@&       @@@ @@         
-    ,@@@,   @@@  @@@  @@@   ,@@@   @@@/        #@@@      ,@@@         #@. @@@ %    /@@@@       (@@  
-            (@@@ @@@ %@@@&  ,@@@   @@@/        (@@@      (@@@            (@@   @@@   @@@            
-                     @@@    ,@@@   @@@/        (@@       @@@@     @@@@   @@@    @@@  @@@            
-                    @@@     ,@@@   @@&       @@@        *@@@      @@@@  @@.          @@@            
-                   %@,      ,@@                         @@@       @@@&           %&..@(             
-                          .@@                         .@@&        @@@,             #(               
-                                                     @&      @   ,@*                                
-                                                               %@,                                  
-                                                                                                    
-    使用本程序后果自行承担      /@@@@@@@@@@@@@@/Auto Arknights/@@@@@@@@@@@@@#           REVUnit 2020   
-                                                                                                    
+        private static readonly Lazy<App> LazyInitializer = new(new App());
 
-";
+        private App() { }
 
-        private static readonly Lazy<App> _lazyInitializer = new(new App());
-
-        private App()
-        {
-            
-        }
-
-        public static App Instance => _lazyInitializer.Value;
+        public static App Instance => LazyInitializer.Value;
         public Config Config { get; } = new(ConfigFilePath);
 
         public void Run()
         {
-            if (!Library.CheckIfSupported()) throw new NotSupportedException("你当前的CPU不支持AVX2指令集，无法运行本程序");
+            if (!Library.CheckIfSupported()) throw new NotSupportedException("CPU不支持AVX2指令集，无法运行本程序");
             var cin = new Cin { AutoTrim = true };
 
-            Console.Write(Logo);
+            Console.WriteLine(Resources.StartupLogo);
             Log.Information("正在初始化设备抽象层");
-            UserInterface.Initialize(Config.Remote_AdbExecutable, Config.Remote_Address);
+            UserInterface.Initialize(Config.RemoteAdbExecutable, Config.RemoteAddress);
             Log.Information("启动成功");
             Console.Clear();
 
-            Parameters parameters = cin.Get(@"<\d: 模式>[\d+: 刷关次数][\w+: 后续操作]", Parameters.Parse) ??
-                                    throw new Exception("意外的EOF");
+            Parameters? prms = cin.Get(@"<模式>[刷关次数][后续操作]", ParseParameters);
+
+            if (prms == null) return;
 
             Console.Write(@"
 -[任务列表]---------------------------------------
 
 ");
-            for (var i = 0; i < parameters.Tasks.Length; i++)
+            for (var i = 0; i < prms.Tasks.Length; i++)
             {
-                IArkTask task = parameters.Tasks[i];
+                IArkTask task = prms.Tasks[i];
                 Console.WriteLine($@"[{i}]> {task}");
             }
 
             Console.Write(@"
 --------------------------------------------------
-
 ");
-            
+
+            Log.Information("即将执行 {tasks}", prms.Tasks);
             XConsole.AnyKey();
-            Log.Information("即将执行{tasks}",parameters.Tasks);
 
-            for (var i = 0; i < parameters.Tasks.Length; i++)
+            for (var taskId = 0; taskId < prms.Tasks.Length; taskId++)
             {
-                IArkTask task = parameters.Tasks[i];
+                IArkTask task = prms.Tasks[taskId];
 
-                Log.Information("任务[{number}]: 任务开始", i);
+                Log.Information("任务[{taskId}]: 任务开始", taskId);
                 ExecuteResult executeResult = task.Execute();
 
-                const string info = "任务[{number}]: {executeResult.Message}";
                 if (executeResult.Succeed)
-                    Log.Information(info);
+                    Log.Information("任务[{taskId}]完成：{message}", taskId, executeResult.Message);
                 else
-                    Log.Error(info);
+                    Log.Error("任务[{taskId}]出现错误：{message}", taskId, executeResult.Message);
             }
 
             XConsole.AnyKey("所有任务完成");
+        }
+
+        private Parameters ParseParameters(string s)
+        {
+            if (s != "help")
+                return Parameters.Parse(s,
+                                        new PostAction.ParseSettings
+                                        {
+                                            Command = Config.RemoteShutdownCommand,
+                                            ForcedSuspend = Config.ForcedSuspend
+                                        });
+
+            Console.WriteLine(Resources.QuickHelpMessage);
+            throw new Exception();
         }
 
         private class Parameters
@@ -105,17 +90,17 @@ namespace REVUnit.AutoArknights.CLI
             public Parameters(IArkTask[] tasks) => Tasks = tasks;
             public IArkTask[] Tasks { get; }
 
-            public static Parameters Parse(string value)
+            public static Parameters Parse(string value, PostAction.ParseSettings parseSettings)
             {
                 var reader = new StringReader(value);
 
                 int modeValue = reader.Read() - '0';
-                var _mode = (FarmLevel.FarmMode) modeValue;
-                if (!Enum.IsDefined(_mode)) throw new ArgumentException("无效模式");
+                var mode = (FarmLevel.FarmMode) modeValue;
+                if (!Enum.IsDefined(mode)) throw new ArgumentException("无效的模式，请输入 \"help\" 来获取快速帮助");
 
                 var tasks = new List<IArkTask>();
 
-                if (_mode == FarmLevel.FarmMode.SpecifiedTimes || _mode == FarmLevel.FarmMode.SpecTimesWithWait)
+                if (mode == FarmLevel.FarmMode.SpecifiedTimes || mode == FarmLevel.FarmMode.SpecTimesWithWait)
                 {
                     var b = new StringBuilder();
                     while (true)
@@ -126,29 +111,25 @@ namespace REVUnit.AutoArknights.CLI
                         if (char.IsDigit((char) v)) b.Append(c);
                     }
 
-                    if (b.Length == 0) throw new ArgumentException("无效的刷关次数值");
+                    if (b.Length == 0) throw new ArgumentException("无效的刷关次数值，请输入 \"help\" 来获取快速帮助");
 
                     int repeatTimes = int.Parse(b.ToString());
 
-                    var task = new FarmLevel(_mode, repeatTimes);
+                    var task = new FarmLevel(mode, repeatTimes);
                     tasks.Add(task);
                 }
                 else
                 {
-                    tasks.Add(new FarmLevel(_mode, -1));
+                    tasks.Add(new FarmLevel(mode, -1));
                 }
 
                 if (reader.Peek() == -1)
                     return new Parameters(tasks.ToArray()); // A mode value and maybe a repeat times number parsed
 
-                string postActions = reader.ReadToEnd();
-                IEnumerable<IArkTask> _postActions = postActions.Select<char, IArkTask>();
-                tasks.AddRange(_postActions);
+                tasks.AddRange(reader.ReadToEnd().Select(c => PostAction.Parse(c, parseSettings)));
 
                 return new Parameters(tasks.ToArray());
             }
-            
-            private static 
         }
     }
 }
