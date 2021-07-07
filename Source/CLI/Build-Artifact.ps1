9125407e0a95adb4af7a1209a32379d6b3841b5b
Rcmcpe <rcmcpe-cn@outlook.com>
2021-07-07 14:14:37 +0800

    Fix build

--- Source/CLI/Build-Artifact.ps1
+++ Source/CLI/Build-Artifact.ps1
@@ -1,3 +1,15 @@
 if (Test-Path .\artifact\) { Remove-Item -Recurse .\artifact\ }
 
-dotnet publish -c Release -p:PublishProfile="Windows x64"
+git version
+if ($?) 
+{
+    $tag = git describe --abbrev=0
+    $version = $tag.Substring(1)
+    $commit = git -c log.showSignature=false log --format=format:%h -n 1
+    $infoVersion = "$version+$commit"
+    dotnet publish -o .\artifact -c Release -p:PublishProfile="Windows x64" -p:Version=$infoVersion
+}
+else
+{
+    dotnet publish -o .\artifact -c Release -p:PublishProfile="Windows x64"
+}
